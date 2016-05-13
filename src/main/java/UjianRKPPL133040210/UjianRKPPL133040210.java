ad42cbe12f89d09b577127d8f484db8ea83192fd
khaerul <khaerul.ikhfan@gmail.com>
2016-05-13 14:07:38 +0700

    UjianRKPPL133040210

---
+++ src/main/java/UjianRKPPL133040210/UjianRKPPL133040210.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UjianRKPPL133040210;
+
+/**
+ *
+ * @author SB604-03
+ */
+public class UjianRKPPL133040210 {
+    
+    public int hitung(int nilai1, int nilai2){
+        int total = nilai1+nilai2;
+    }
+}
